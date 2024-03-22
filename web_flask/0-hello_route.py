6a796563d4087adf0e3a75cf65a3cf8181f56063
Sam-d23 <samkiragu05@gmail.com>
2024-03-22 03:58:13 -0700

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Route displaying 'Hello HBNB!'"""
+    """Route that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
