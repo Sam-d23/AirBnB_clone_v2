be68f43501e4b346e489f187978f78402cd896c5
Sam-d23 <samkiragu05@gmail.com>
2024-04-17 09:50:37 +0300

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,5 +15,7 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
+    #The Flask development server is started
+    #Listens on all network interfaces (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000)
 
