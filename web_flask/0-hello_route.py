8c97a3687ed1e88e2f2146837a72a5feec6d3e69
Sam-d23 <samkiragu05@gmail.com>
2024-04-18 17:54:30 +0300

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,14 +8,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Route that displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    #The Flask development server is started
-    #Listens on all network interfaces (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host="0.0.0.0")
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
     """Route displaying 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
